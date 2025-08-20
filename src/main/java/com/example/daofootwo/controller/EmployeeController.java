9ab6e3373fc058edd6badbc8a6456449c621c533
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-20 18:55:46 -0400

    Creada solicitud PUT /employees para editar un empleado

--- src/main/java/com/example/daofootwo/controller/EmployeeController.java
+++ src/main/java/com/example/daofootwo/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,17 @@ public class EmployeeController {
     try {
       EmployeeDTO foundEmployee = employeeService.getEmployeeById(employeeId);
       return ResponseEntity.ok(foundEmployee);
+    } catch (Exception ex) {
+      System.err.println("Error: " + ex);
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @PutMapping
+  public ResponseEntity<EmployeeDTO> updateEmployee(@RequestBody EmployeeDTO employeeDTO) {
+    try {
+      EmployeeDTO updatedEmployee = employeeService.saveOrUpdateEmployee(employeeDTO);
+      return ResponseEntity.ok(updatedEmployee);
     } catch (Exception ex) {
       System.err.println("Error: " + ex);
       return ResponseEntity.notFound().build();
