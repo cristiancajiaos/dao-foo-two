b888cbb1685c7b0046d3a4d73fbf71d0305b44ca
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-20 17:38:44 -0400

    Primer commit

---
+++ src/main/java/com/example/daofootwo/DaoFooTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.daofootwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoFooTwoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DaoFooTwoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/daofootwo/DaoFooTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daofootwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoFooTwoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
